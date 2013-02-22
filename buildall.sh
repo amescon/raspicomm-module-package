35579230239272e35d797ccd385fb1863e896abb
mdk <mdk@amescon.com>
2013-02-22 19:17:00 +0100

    buildall.sh builds the Packages.gz for the apt-get server as well

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
+# remove all old packages
 rm *.deb
+
+# build the packages
 ./buildpackage.sh 3.2.27+
 ./buildpackage.sh 3.6.11+
+
+# build the override file for dpkg-scanpackges
+ls -l *.deb > override
+
+# create the package file
+dpkg-scanpackages ./ override | gzip > Packages.gz
