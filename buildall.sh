10faf5d3385d898d28afe638e0ec72ec77847ae7
Martin <mdk@amescon.com>
2014-06-24 14:42:14 +0200

    updated script for newer versions

--- buildall.sh
+++ buildall.sh
@@ -10,6 +10,8 @@ rm *.deb
 ./buildpackage.sh 3.10.19+ 600
 ./buildpackage.sh 3.10.24+ 614
 ./buildpackage.sh 3.10.25+ 622
+./buildpackage.sh 3.10.38+ 675
+./buildpackage.sh 3.12.22+ 691
 
 # copy the packages to the global package folder
 cp *.deb ../Documents/packages
