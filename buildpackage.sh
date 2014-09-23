cd97c5a3181e35c32b9a10ae90f45092e2f65d69
Martin <mdk@amescon.com>
2014-09-23 11:38:34 +0200

    updated buildpackage.sh to support packages that reuse binaries

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,22 +1,33 @@
 # use the first parameter as the version of the package
-if [ -z $1 ] ; then
-  echo No Version specified as an argument, using version 3.6.11+  
+if [[ -z $1 ]]; then
+  echo "No Version specified as an argument, using version 3.6.11+"
   VERSION="3.6.11+"
 else
   VERSION=$1
 fi
 
-if [ -z $2 ] ; then
-  echo No Subversion specified as an argument, using version as name
+# use the 2nd parameter as subversion
+if [[ -z $2 ]]; then
+  echo "No Subversion specified as an argument, using version as name"
   NAME="$VERSION"
 else
 	NAME=$1$2
 fi
 
+# if arguments 3 & 4 are specified, use them as the binary
+if [[ ! -z $3 && ! -z $4 ]]; then
+  echo "use ${3} ${4} as the binary base"
+  MAKEDIR=${3}${4}
+else
+  MAKEDIR=${NAME}
+fi
+
 # execute make for the module
-make -C ../raspicomm-module ${NAME}
+make -C ../raspicomm-module ${MAKEDIR}
 
 MODULESDIR="./debian/lib/modules"
+
+# kernel module directory
 KODIR="${MODULESDIR}/${VERSION}"
 
 # remove old debian/lib/modules folders
